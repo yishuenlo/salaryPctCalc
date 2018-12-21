3b23d4b44810e92e050c560530ecf726b71dcf1b
Yishuen Lo <lo.yishuen@gmail.com>
2018-12-20 19:35:32 -0700

    init

---
+++ script.js
@@ -0,0 +1,32 @@
+const inflation = 0.0226; //2.26%
+var calcDisplay = document.getElementById("calc-result");
+var startSalaryDisplay = document.getElementById("start_salary");
+var currentSalaryDisplay = document.getElementById("current_salary");
+var expDisplay = document.getElementById("experience");
+
+
+//initial dispaly of "---"
+calcDisplay.textContent = "---";
+
+function calcPercent(startingSalary, currentSalary, yearsOfExperience){
+  var startingSalary = Number(startSalaryDisplay.value);
+  var currentSalary = Number(currentSalaryDisplay.value);
+  var yearsOfExperience = Number(expDisplay.value);
+
+  //calcuation
+  var percentIncrease = (Math.pow(currentSalary/startingSalary, 1/yearsOfExperience)- 1 - inflation) * 100;
+
+  //update display
+  calcDisplay.textContent = parseFloat(percentIncrease).toFixed(1);
+};
+
+expDisplay.onkeyup = function() {calcPercent()};
+currentSalaryDisplay.onkeyup = function() {calcPercent()};
+startSalaryDisplay.onkeyup = function() {calcPercent()};
+
+// if(Number(expDisplay.value) > 0){
+//   expDisplay.onkeyup = function() {calcPercent()};
+//   currentSalaryDisplay.onkeyup = function() {calcPercent()};
+//   startSalaryDisplay.onkeyup = function() {calcPercent()};
+//   alert(expDisplay.value);
+// };
